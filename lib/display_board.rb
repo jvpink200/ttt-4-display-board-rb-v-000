1ea0f952f826be415412d430112ad3ba43e0e133
jvpink200 <jvillarr20@gmail.com>
2018-09-08 23:28:51 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  puts row1 = "   |   |   "
+  puts row2 = "-----------"
+  puts row3 = "   |   |   "
+  puts row4 = "-----------"
+  puts row5 = "   |   |   "
+end
+
+puts "#{board[0]}"
